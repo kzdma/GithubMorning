852cee23bfe4a6ffc4443a30810c423e4d704b48
Konstantin <konstant_z@yahoo.com>
2026-03-20 11:07:04 +0200

    SignIn Activity Create Initialize Views Method

---
+++ SignInActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GithubMorning
+{
+	[Activity(Label = "SignInActivity")]
+	public class SignInActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			//Hello from SignInActivity
+
+			InitializeViews();
+		}
+
+		private void InitializeViews()
+		{
+			//Initialize views here
+			throw new NotImplementedException();
+		}
+	}
+}
