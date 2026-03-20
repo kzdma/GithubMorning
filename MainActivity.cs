7051a76446f4768dbcb9f07e618685062b1b6d2a
Konstantin <konstant_z@yahoo.com>
2026-03-20 10:58:59 +0200

    Revert "Add button to MainActivity"
    This reverts commit 0bbe4d871e2d11c652b7dcd03d8507fdb97762b5.

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
-using Android.Widget;
 using AndroidX.AppCompat.App;
 using System;
 
@@ -23,10 +22,6 @@ namespace GithubMorning
 		private void InitializeViews()
 		{
 			//If you want to initialize your views, you can do it here. For example:
-            Button btn = FindViewById<Button>(Resource.Id.myButton);
-
-
-
 			throw new NotImplementedException();
 		}
 
